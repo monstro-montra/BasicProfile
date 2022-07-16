e4984f05100ae2cc8b4437e504eba33ab5515895
monstro-montra <alexjavier1337@gmail.com>
2022-07-16 03:53:57 -0400

    Test

---
+++ BasicProfile/src/main/Profile.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Profile {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
---
+++ BasicProfile/src/module-info.java
@@ -0,0 +1,3 @@
+module BasicProfile {
+	exports main;
+}
