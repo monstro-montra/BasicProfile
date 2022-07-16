64abc68f937bd54805b43d6eec86acfda3f1c8eb
monstro-montra <alexjavier1337@gmail.com>
2022-07-16 04:39:42 -0400

    Basic Scanner

--- BasicProfile/src/main/Profile.java
+++ BasicProfile/src/main/Profile.java
@@ -1,10 +1,23 @@
 package main;
+import java.util.Scanner;
 
 public class Profile {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world2!");
+		Scanner in = new Scanner (System.in);
+		
+		
+		
+		System.out.println("Insert your first name:");
+		
+		String firstName = in.nextLine();
+		
+		System.out.println("Insert your last name:");
+		
+		String lastName = in.nextLine();
+		
+		System.out.println("Your full name is: " + firstName + " " + lastName);
 
 	}
 
