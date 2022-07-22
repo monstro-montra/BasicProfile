0cae6e93ab2d227fafd398dc4381cbb836fe5a78
monstro-montra <alexjavier1337@gmail.com>
2022-07-22 02:05:55 -0400

    Used try and finally function to fix the scanner resource leak error.

--- BasicProfile/src/main/Profile.java
+++ BasicProfile/src/main/Profile.java
@@ -1,23 +1,31 @@
 package main;
-import java.util.Scanner;
+import java.util.Scanner; //import scanner to allow user for user input
 
-public class Profile {
+public class Profile { //name of project
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main method for java
 		
-		Scanner in = new Scanner (System.in);
+		Scanner in = null; //creates a new variable called 'in' with the type being Scanner.
+
+		try{
+			in = new Scanner (System.in);
+			System.out.println("Insert your first name:");
 		
+			String firstName = in.nextLine();
+			
+			System.out.println("Insert your last name:");
+			
+			String lastName = in.nextLine();
+			
+			System.out.println("Your full name is: " + firstName + " " + lastName);
+		}
+
+		finally{
+			if(in != null){
+				in.close();
+			}
+		}
 		
-		
-		System.out.println("Insert your first name:");
-		
-		String firstName = in.nextLine();
-		
-		System.out.println("Insert your last name:");
-		
-		String lastName = in.nextLine();
-		
-		System.out.println("Your full name is: " + firstName + " " + lastName);
 
 	}
 
