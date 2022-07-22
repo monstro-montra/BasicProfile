1b824035cfa3825658be8025555b080796154a00
monstro-montra <alexjavier1337@gmail.com>
2022-07-22 03:52:06 -0400

    added comments and moved string declarations to the beginning. added zip code, city, and address.

--- BasicProfile/src/main/Profile.java
+++ BasicProfile/src/main/Profile.java
@@ -4,27 +4,53 @@ import java.util.Scanner; //import scanner to allow user for user input
 public class Profile { //name of project
 
 	public static void main(String[] args) { //main method for java
+
+		String firstName; //uninitiated string named firstName
+		String lastName; //uninitiated string named lastName
+		String streetAddress; //uninitiated string named streetAddress
+		String userCity; //uninitiated string named userCity
+		String zipCode; //uninitiated string named zipCode
+
 		
 		
 /*The following statement will a new variable called 'in' with the type being Scanner. 
  * Using the try with resource statement, I can avoid a resource leak error where
- * the scanner never closes. For reference, use the following URL.
+ * the scanner never closes. "A resource is an object that must be closed after the program is finished with it.
+ * The try-with-resources statement ensures that each resource is closed at the end of the statement."
+ * For reference, use the following URL.
  * https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
 */
 
-		try(Scanner in = new Scanner (System.in)){
-			System.out.println("Insert your first name:");
+		try(Scanner in = new Scanner (System.in)){ //create new scanner called 'in'
+			System.out.print("Input your first name: "); //ask the user to input their first name
 		
-			String firstName = in.nextLine();
+			firstName = in.nextLine(); //assign next input to firstName
 			
-			System.out.println("Insert your last name:");
+			System.out.print("Input your last name: "); // asks the user to input their last name
 			
-			String lastName = in.nextLine();
+			lastName = in.nextLine(); //assign next input to lastName
 			
-			System.out.println("Your full name is: " + firstName + " " + lastName);
-		}
+			System.out.print("Input your street address: "); //ask the user to input their street address
 
-	
+			streetAddress = in.nextLine(); //assign next input to streetAddress
+
+			System.out.print("Input your city: ");
+
+			userCity = in.nextLine(); //assign next input to userCity
+
+			System.out.print("Input your zip code: ");
+
+			zipCode = in.nextLine(); //assign next input to zipCode
+
+			System.out.println("Here is your profile!");
+			System.out.println("First name: " + firstName); //print value of firstName
+			System.out.println("Last name: " + lastName); //print value of lastName
+			System.out.println("Street Address: " + streetAddress); //print value of streetAddress
+			System.out.println("City: " + userCity); //print value of userCity
+			System.out.println("Zip Code: " + zipCode); //print value of zipCode
+			System.out.println("You may now close this program.");
+
+		}
 	}
 
 }
